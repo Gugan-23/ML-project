a949988bfbb4b6738dc05e0260770c3485c3f1bf
Gugan <v.gugan16@gmail.com>
2024-10-16 12:25:34 +0530

    Initial commit

--- ML-Login-page-main/frontend/src/components/AboutUs.js
+++ ML-Login-page-main/frontend/src/components/AboutUs.js
@@ -1,5 +1,8 @@
+// AboutUs.js
+
 import React from 'react';
 import './AboutUs.css'; // Import the CSS file
+import MovableGraph from './MovableGraph'; // Import the MovableGraph component
 
 const AboutUs = () => {
     return (
@@ -7,7 +10,11 @@ const AboutUs = () => {
             <h1>About Us</h1>
             <p>We are dedicated to promoting environmental awareness through e-waste management.</p>
             <p>Our mission is to educate the public on the importance of recycling e-waste responsibly.</p>
-            
+
+            {/* Add the MovableGraph component here */}
+            <div className="movable-graph">
+                <MovableGraph />
+            </div>
         </div>
     );
 };
